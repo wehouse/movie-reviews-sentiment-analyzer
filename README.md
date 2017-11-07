beaa5b6b991259c4e35abd1a33c0d4ae85d26956
Daniel Yinanc <daniel.yinanc@gmail.com>
2017-11-07 09:45:36 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# movie-reviews-sentiment-analyzer
+# Movie Reviews Sentiment Analyzer
+
+## Introduction
+By using a well known dataset (Rotten Tomatoes reviews set). We will be demonstrating how to create a Natural Language Processing
+classification system. Also our secondary aim is to demonstrate how to correctly functionalize such as a system using
+microservices architecture
+
+## Dataset
+By using Pang and Lee's polarity sentiment dataset V2.0 [1]. This dataset consists of 2000 movie reviews that are painstakingly
+categorized as positive and negative by authors. 
+
+## Data Pre-processing
+Dataset generously contributed by Pang and Lee is quite high quality in its completeness. All reviews are inside text files
+that are uniquely idenfied via a notification
+
+## Models
+In order to analyze any dataset, we first need to have a model of explanation in mind. Each model has its strengths and
+weaknesses which must be carefully evaluated.
+
+In order of implementation, we used the following models:
+1. Naive Bayes
+
+## Results
+In order to compare results of multiple models, we will be using the standard confusion matrix in order to evaluate test vs pred
+accuracy. We will introduce other techniques of comparison in time.
+
+### Naive Bayes Classifier
+Confusion matrix yielded
+
+|   	| 0  	| 1  	|
+|---	|---	|---	|
+| 0  	| 163  	| 37  	|
+| 1  	| 67  	| 133  	|
+
+This translates into accuracy of correctly predicting test values by 296/400 ~ %75
+
+## References
+1. This data was first used in Bo Pang and Lillian Lee,
+``A Sentimental Education: Sentiment Analysis Using Subjectivity Summarization 
+Based on Minimum Cuts'',  Proceedings of the ACL, 2004.
---
+++ model_naive_bayes.py
@@ -0,0 +1,77 @@
+# Natural Language Processing
+
+# Importing the libraries
+import glob
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from numpy import array
+
+# Importing the dataset
+#training_dataset = pd.read_csv('train.tsv', delimiter = '\t', quoting = 3)
+data=[]
+filepath = "txt_sentoken/pos/*.txt"
+txt = glob.glob(filepath)
+for textfile in txt:
+    with open(textfile, 'r') as myfile:
+        tmp_arr=[]
+        tmp_arr.append(myfile.read().replace('\n', ''))
+        data.append(tmp_arr)
+data=array(data)
+data_pos=np.insert(data,1,1,axis=1)
+
+data=[]
+filepath = "txt_sentoken/neg/*.txt"
+txt = glob.glob(filepath)
+for textfile in txt:
+    with open(textfile, 'r') as myfile:
+        tmp_arr=[]
+        tmp_arr.append(myfile.read().replace('\n', ''))
+        data.append(tmp_arr)
+data=array(data)
+data_neg=np.insert(data,1,0,axis=1)
+
+data_combined=np.append(data_pos,data_neg,axis=0)
+
+dataset = pd.DataFrame(data=data_combined,columns=['Review','Score'])
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 2000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 2000)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
